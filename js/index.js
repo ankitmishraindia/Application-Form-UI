c015f3fafccfe75692749517b599af58056f8d8e
ankit mishra <mishraankit987@gmail.com>
2024-04-01 13:30:25 +0530

    improved form with heading icon and placeholders

--- js/index.js
+++ js/index.js
@@ -242,7 +242,7 @@ function validateForm(){
 const buttons=document.getElementsByTagName('button')
 
 //click event
-for(let i=1;i<8;i++){
+for(let i=1;i<9;i++){
     buttons[i].addEventListener('click',function(){
         // Get the target section ID
       const targetId = buttons[i].getAttribute('data-target');
